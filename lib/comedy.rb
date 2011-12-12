02d77f711b32e26d15e9ea3ea7e1415deb6250a2
anand <anandvasudevan9@gmail.com>
2011-12-13 00:20:37 +0530

    testing with core

--- lib/comedy.rb
+++ lib/comedy.rb
@@ -1,7 +1,14 @@
 module Comedy
-  #require '
-  def self.comedy
-    Launchy.open("http://www.google.com")
-  end 
- 
+  module TellComedy
+    def mycomedy
+      Launchy.open("http://www.google.com")
+    end 
+ end
 end
+
+class Object
+  def self.Comedy
+    include Comedy::TellComedy
+  end
+end
+
