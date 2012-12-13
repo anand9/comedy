b5151c6b8882aa14cff26fcf931f1a07e38275c1
anand vasudevan <anandvasudevan9@gmail.com>
2012-12-13 12:08:18 +0530

    Add rakefile and gitignore

--- lib/comedy.rb
+++ lib/comedy.rb
@@ -1,8 +1,8 @@
 require 'launchy'
 module Comedy
   #require '
-  def self.i_need_fun
-    Launchy.open("http://www.google.com")
+  def self.i_need_fun(star)
+    Launchy.open("http://www.youtube.com/results?search_query=comedy+#{star}")
   end 
  
 end
